949fc6ff4b7e4c0c48a8bba349804a808f7c91ae
Davidgc16 <davidgerecomi@uma.es>
2021-11-12 18:35:53 +0100

    Resumen abajo
    Este commit parece que hace la conexión a la base de datos de forma correcta, no he conseguido ni insertar ni leer un objeto, si parece que se trae algo de la base de datos.

--- APP/app.py
+++ APP/app.py
@@ -3,12 +3,20 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)	
 
-app.config["MONGO_URI"] = "mongodb+srv://Gestionpymongo:<Gestionpymongo>@cluster0.iixvr.mongodb.net/iweb?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://Gestionpymongo:Gestionpymongo@cluster0.iixvr.mongodb.net/iweb?retryWrites=true&w=majority"
 mongo = PyMongo(app)
-
+db = mongo.db
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    
+    
+    doc = {'paco'}
+    t = db['test']
+    a = t.insert_one(doc)
+
+    return "insertado"
+
+  
 
 if __name__ == '__main__':
    		app.run()
