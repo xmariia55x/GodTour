552a871bbeb507067514b5c46737cd8630eaa200
YerayRuiz <yeray.r7@uma.es>
2022-01-13 17:01:13 +0100

    add base.js

--- APP/static/js/base.js
+++ APP/static/js/base.js
@@ -14,7 +14,7 @@ function onSignIn(googleUser) {
     };
     xhr.send('idtoken=' + id_token);
 }
-
+////
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then( function () { console.log('User signed out.'); } );
